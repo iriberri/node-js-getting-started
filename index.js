cb2c555e9f9b0e468557d51bc3366ae9d7ad5a64
Carla <iriberrig@gmail.com>
2019-07-03 16:03:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+console.log('.'.repeat(100));
+console.log(process.env);
+
+console.log(process.env.GATSBY_TELEMETRY_DISABLED);
+console.log('.'.repeat(100));
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
