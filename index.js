1f9113ff7af92a18521423d2a4bd17ae633f1a7f
Carla <iriberrig@gmail.com>
2019-12-11 16:51:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const PORT = process.env.PORT || 5000
 console.log('.'.repeat(100));
 console.log(process.env);
 
+var url   = require('url');
+var Redis = require('ioredis');
+
+redis_uri = url.parse(process.env.REDIS_URL);
+var redis = new Redis({
+  port: Number(redis_uri.port) + 1,
+  host: redis_uri.hostname,
+  password: redis_uri.auth.split(':')[1],
+  db: 0,
+  tls: {
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false
+  }
+});
+
+console.log(redis);
+
+redis.connect();
+
 console.log(process.env.GATSBY_TELEMETRY_DISABLED);
 console.log('.'.repeat(100));
 
